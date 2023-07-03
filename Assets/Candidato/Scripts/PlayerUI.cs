b6bf4f32e6b3f5bdb1a91cda7079cde570677951
DamianJimenoRodriguez <damian.jimeno@everguild.com>
2023-07-03 14:56:57 +0200

    Added Timer

--- Assets/Candidato/Scripts/PlayerUI.cs
+++ Assets/Candidato/Scripts/PlayerUI.cs
@@ -9,10 +9,17 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI TimeRemainingText;
     [SerializeField] private Player player;
+    [SerializeField] private Timer timer;
 
     private void Awake()
     {
         player.OnPlayerScore += UpdateScoreText;
+        timer.OnTick += UpdateTimeRemainingText;
+    }
+
+    private void UpdateTimeRemainingText(float timeRemaing)
+    {
+        TimeRemainingText.text = timeRemaing.ToString("0:00.00");
     }
 
     private void UpdateScoreText(int currentScore)
---
+++ Assets/Candidato/Scripts/Timer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float countDownTime = 60f;
+    [SerializeField] private bool isTicking;
+
+    private float remainingTime;
+
+    public System.Action<float> OnTick;
+    public System.Action OnTimerEnd;
+
+    private void Awake()
+    {
+        remainingTime = countDownTime;
+    }
+
+    private void Update()
+    {
+        if (isTicking)
+        {
+            Tick();
+        }
+    }
+
+    public void Tick()
+    {
+        remainingTime -= Time.deltaTime;
+        if (remainingTime > 0)
+        {
+            if (OnTick != null)
+            {
+                OnTick(remainingTime);
+            }
+        }
+        else
+        {
+            if (OnTick != null)
+            {
+                OnTick(0);
+            }
+            if (OnTimerEnd != null)
+            {
+                OnTimerEnd();
+            }
+            isTicking = false;
+        }
+    }
+}
